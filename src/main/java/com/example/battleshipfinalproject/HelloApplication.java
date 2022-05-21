f68bdd48377cbf6743e81a5d986fc306f62ce369
LaurianeDegue <81866531+LaurianeDegue@users.noreply.github.com>
2022-05-21 01:11:19 -0400

    Starting the Board class

--- src/main/java/com/example/battleshipfinalproject/HelloApplication.java
+++ src/main/java/com/example/battleshipfinalproject/HelloApplication.java
@@ -1,7 +1,6 @@
 package com.example.battleshipfinalproject;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,8 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene();
         stage.setTitle("JavaFX Battleship");
 
         VBox boards = new VBox();
